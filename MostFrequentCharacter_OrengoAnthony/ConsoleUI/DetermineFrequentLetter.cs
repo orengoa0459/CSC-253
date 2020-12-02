b42c56c083e714b2109a7974eb77e4288ec2d7bb
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-12-02 18:48:13 -0500

    Add files via upload

---
+++ MostFrequentCharacter_OrengoAnthony/ConsoleUI/DetermineFrequentLetter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordLibrary;
+namespace ConsoleUI
+{
+    public class DetermineFrequentLetter
+    {
+        public static void DetermineTheFrequentCharacter(ref string input)
+        {
+            Console.Clear();
+            // Declare and initialize array for aschii characters
+            int[] aschiiCharacters = new int[256];
+            
+            //Count each character in the string 
+            for (int i = 0; i < input.Length; i++)
+            {
+                aschiiCharacters[input[i]]++;               
+            }
+            //Base counter for character that appears the most
+            int count = -1;
+            char letter = ' ';
+            //Determine which letter appears the most in the string
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (count < aschiiCharacters[input[i]])
+                {
+                    count = aschiiCharacters[input[i]];
+                    letter = input[i];
+                }
+            }
+            //Display results to user
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayResults());
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayFrequentLetter(input, letter, count));            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MostFrequentCharacter_OrengoAnthony/ConsoleUI/EnterString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordLibrary;
+namespace ConsoleUI
+{
+    public class EnterString
+    {
+        public static void EnterAStringOfWords(ref string input)
+        {
+            //Clear current screen
+            Console.Clear();
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayEnterWordsTitle());
+            //Get string of words from user
+            Console.Write (WordLibrary.StandardMessages.DisplayEnterWords());
+            input = Console.ReadLine();
+            //Remove whitespace
+            input = input.Trim();
+        }
+    }
+}
