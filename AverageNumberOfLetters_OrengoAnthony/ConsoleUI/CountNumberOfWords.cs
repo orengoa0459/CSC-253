418e64411a27bc12673d7cd9d555b81135b67bde
orengoa0459 <48802353+orengoa0459@users.noreply.github.com>
2020-12-02 18:47:49 -0500

    Add files via upload

---
+++ AverageNumberOfLetters_OrengoAnthony/ConsoleUI/CountNumberOfWords.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class CountNumberOfWords
+    {
+        public static void CountWords(ref string sentence, ref string[] token)
+        {
+            Console.Clear();
+            //Display number of words to user
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayAverageLettersTitle());
+            Console.WriteLine($"Number of Words: {token.Length}");                       
+        }
+
+        public static void CountNumberOfLetters(ref string sentence, ref int lengthLetters)
+        {
+            //Calculate number of letters and display to the user
+            int x = 0;
+            lengthLetters = 0;
+            while (x <= sentence.Length - 1)
+            {
+                if (sentence[x] == ' ' || sentence[x] == '\n' || sentence[x] == '\t')
+                {
+
+                }
+                else
+                {
+                    lengthLetters++;
+                }
+                x++;
+            }            
+            Console.WriteLine($"Total Letters: {lengthLetters}");
+        }
+
+        public static void GetAverageLetters(ref string sentence, ref string[] token, ref int lengthLetters)
+        {
+            //Calculate the average number of letters in each word
+            int averageLetters;
+            averageLetters = lengthLetters / token.Length;           
+            Console.WriteLine($"\nAverage Letters:  {averageLetters}");
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayPressEnter());
+        }
+    }
+}
---
+++ AverageNumberOfLetters_OrengoAnthony/ConsoleUI/GetString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class GetString
+    {
+        public static void GetSentenceFromUser(ref string sentence, ref string[] token)
+        {
+            Console.Clear();
+            //Get string from user and tokenize it by delimitters
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayEnterWordsTitle());
+            Console.Write(WordLibrary.StandardMessages.DisplayEnterWords());
+            sentence = Console.ReadLine();
+            //Tokenize string and split into an array
+            token = sentence.Split(' ', '.', '\n', '\t', '!', '?');
+            foreach (var e in token)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+            Console.WriteLine(WordLibrary.StandardMessages.DisplayPressEnter());
+        }
+    }
+}
